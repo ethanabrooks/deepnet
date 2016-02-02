c8898eae35ea4b012a451a57550e43f66351b869
Ethan <ethanabrooks@gmail.com>
2016-02-02 00:17:31 -0500

    compiles

--- src/Model.hs
+++ src/Model.hs
@@ -8,20 +8,19 @@
 - parallelize
 -}
 
-module Model ( linearLayer
-             , linearLayerFromMatrix
-             , sequentialNet
-             , sequentialNetFromMatrices
-             , sigmoid
-             , sigmoidInput
-             , weights
-             , input
-             , children
-             , backprop
-             , Network
+module Model (
+             {-linearLayer-}
+             {-, linearLayerFromMatrix-}
+             {-, sigmoid-}
+             {-, sigmoidInput-}
+             {-, getWeights-}
+             {-, input-}
+              backprop
+             , Layer
+             , add
+             , (<>)
              , feedThru ) where
 import           Util
-{-import           Control.Monad.State.Lazy-}
 import           Data.Array.Repa hiding (map,
                                         transpose,
                                         transpose2S,
@@ -32,30 +31,88 @@ import           Data.List (foldl')
 import           Data.Maybe
 import           Control.Monad (join)
 import           Prelude hiding (sequence, zipWith, (++))
-import Debug.Trace
+import           Control.Applicative (Applicative(..))
+import           Control.Monad       (liftM, ap)
+import           Data.Monoid         ((<>))
+import           Debug.Trace
 
 type Input   = Matrix
 type Output  = Matrix
 type Targets = Matrix
 type Error   = Matrix
+data Network = Network (Input -> (Output, Error -> (Network, Error)))
 
-class Network a where
-    size     :: a -> Int
-    feedThru :: a -> Input -> (a, Output)
-    backprop :: a -> Double -> Error -> (a, Error)
+class Layer a where
+    feedThru        :: a -> Input -> Output
+    backprop        :: a -> Input -> Error -> Error
+    update  :: a -> Double -> Error -> (Network, Error)
 
-class (Network a) => Weighted a where
-    getWeights :: a -> Matrix
-    gradient   :: a -> Matrix -> Matrix
+class (Layer a) => Learner a where
+    getWeights  :: a -> Matrix
+    setWeights  :: a -> Matrix -> a
+    gradient    :: a -> Input -> Error -> Matrix
 
-data Model a =
-  Trained     { network      :: a
-              , costFunction :: Output -> Targets -> Error
-              , learningRate :: Double }
+update' :: Learner a => a -> Double -> Input -> Error -> (a, Error)
+update' layer learningRate input error =
+  (layer', backprop layer input error)
+    where layer'   = setWeights layer weights'
+          weights' = addGradients learningRate grad (getWeights layer)
+          grad     = gradient layer input error
 
-  | Untrained { buildNetwork :: Int -> Int -> a
-              , costFunction :: Output -> Targets -> Error
-              , learningRate :: Double }
+instance Monoid Network where
+    mempty = Network (\ input -> (input, (\ error -> (mempty, error))))
+    mappend (Network net1) (Network net2) = Network (\ input ->
+      let (output1, backprop1) = net1 input
+          (output2, backprop2) = net2 output1
+      in  (output2, \ error ->
+          let (net2', error2) = backprop2 error
+              (net1', error1) = backprop1 error1
+          in  (net1' <> net2', error1)))
+
+data Model = Model { network      :: Network
+                   , costFunction :: Output -> Targets -> Error
+                   , learningRate :: Double }
+
+train :: Input -> Targets -> Model -> Int -> Model
+train input targets model numEpochs =
+    iterate (epoch input targets) model !! numEpochs
+
+epoch :: Input -> Targets -> Model -> Model
+epoch input targets (Model (Network process) costFunction learningRate) =
+    Model network' costFunction learningRate
+    where (output, backprop) = process input
+          error              = costFunction output targets
+          (network', _)      = backprop error
+
+add :: Layer a => a -> Double -> Input -> Network
+add layer learningRate input = Network (\ input ->
+                                       (feedThru layer input, (\ error ->
+                                       update layer learningRate error)))
+
+addGradients :: Double -> Matrix -> Matrix -> Matrix
+addGradients learningRate gradient weights =
+    weights + rmap (*learningRate) gradient
+
+{-instance Functor Signal where-}
+    {-fmap = liftM-}
+
+{-instance Applicative Signal where-}
+    {-pure input = Signal input (\ error -> (pure, error))-}
+    {-(<*>) = ap-}
+
+{-instance Monad Signal where-}
+    {-return = pure-}
+    {-Signal input backprop >>= f =-}
+      {-let Signal output backprop2 = f input-}
+      {-in  Signal output (\ error ->-}
+        {-let (network2, error2) = backprop2 error-}
+            {-(network1, error1) = backprop error2-}
+        {-in  (network1 >=> network2, error1))-}
+
+
+  {-| Untrained { buildLayer :: Int -> Int -> a-}
+              {-, costFunction :: Output -> Targets -> Error-}
+              {-, learningRate :: Double }-}
 
 {-test :: Model -> Input -> Targets -> (Targets -> Output -> Double)-}
 {-test model input targets scoreFunc numFolds = mean $ map score folds-}
@@ -69,66 +126,48 @@ data Model a =
 {-accuracy targets output = mean $ zipWith equals targets ouput-}
   {-where x `equals` x = 1-}
         {-_ `equals` _ = 0-}
+{-getNet :: (Layer a) => Input -> Targets -> Model a -> a-}
+{-getNet input targets (Untrained buildLayer _ _) =-}
+    {-buildLayer sizeIn sizeOut-}
+    {-where Z :. _ :. sizeIn  = extent input-}
+          {-Z :. _ :. sizeOut = extent targets-}
+{-getNet _ _ model = network model-}
 
-train :: Network a => Input -> Targets -> Model a -> Int -> Model a
-train input targets model numEpochs =
-    iterate (update input targets) model !! numEpochs
+{-data SequentialNet a = SequentialNet { sizeSN   :: Int-}
+                                     {-, children :: [a] }-}
 
-update :: Network a => Input -> Targets -> Model a -> Model a
-update input targets model =
-    Trained network'' costFunc learnRate
-    where (costFunc, learnRate) = (costFunction model, learningRate model)
-          network               = getNet input targets model
-          (network', output)    = feedThru network input
-          error                 = costFunc output targets
-          (network'', _)        = backprop network' learnRate error
-
-getNet :: (Network a) => Input -> Targets -> Model a -> a
-getNet input targets (Untrained buildNetwork _ _) =
-    buildNetwork sizeIn sizeOut
-    where Z :. _ :. sizeIn  = extent input
-          Z :. _ :. sizeOut = extent targets
-getNet _ _ model = network model
-
-addGradients :: (Weighted a) => Double -> Matrix -> a -> Matrix
-addGradients learningRate gradient' network =
-    getWeights network + rmap (*learningRate) gradient'
-
-data SequentialNet a = SequentialNet { sizeSN   :: Int
-                                     , children :: [a] }
-
-sequentialNet :: (Network a) => (Int -> Int -> a) ->
-  [Int -> a] -> Int -> Int -> SequentialNet a
-sequentialNet head tail sizeIn sizeOut = SequentialNet
-  { children = let build (built, sizeOut) buildChild =
-                      let child = buildChild sizeOut
-                      in  (child:built, size child)
-                   (tail', _) = foldl' build ([], sizeOut) $ reverse tail
-               in  head sizeIn sizeOut:tail' }
+{-sequentialNet :: (Layer a) => (Int -> Int -> a) ->-}
+  {-[Int -> a] -> Int -> Int -> SequentialNet a-}
+{-sequentialNet head tail sizeIn sizeOut = SequentialNet-}
+  {-{ children = let build (built, sizeOut) buildChild =-}
+                      {-let child = buildChild sizeOut-}
+                      {-in  (child:built, size child)-}
+                   {-(tail', _) = foldl' build ([], sizeOut) $ reverse tail-}
+               {-in  head sizeIn sizeOut:tail' }-}
 
 
-sequence :: Network a => (a -> Matrix -> (a, Matrix)) ->
-  Matrix -> [a] -> SequentialNet a -> (SequentialNet a, Matrix)
-sequence function signal children network =
-      (network { children = children' }, out)
-      where (children', out) = foldl' propogate ([], signal) $ children
-            propogate (children, signal) child = (child':children, signal')
-                       where (child', signal') = function child signal
+{-sequence :: Layer a => (a -> Matrix -> (a, Matrix)) ->-}
+  {-Matrix -> [a] -> SequentialNet a -> (SequentialNet a, Matrix)-}
+{-sequence function signal children network =-}
+      {-(network { children = children' }, out)-}
+      {-where (children', out) = foldl' propogate ([], signal) $ children-}
+            {-propogate (children, signal) child = (child':children, signal')-}
+                       {-where (child', signal') = function child signal-}
 
-{-neuralNetLayer :: Network a => Int -> Int -> SequentialNet a-}
+{-neuralNetLayer :: Layer a => Int -> Int -> SequentialNet a-}
 {-neuralNetLayer = sequentialNet linearLayer [sigmoid]-}
 
--- sequentialNet linearyLayer [linearLayer 3, linearLayer2]
+{--- sequentialNet linearyLayer [linearLayer 3, linearLayer2]-}
 
 
-instance Network a => Network (SequentialNet a) where
-  size = sizeSN
-  feedThru network input =
-    (net { children = reverse $ children net }, output)
-    where (net, output) = sequence feedThru input (children network) network
-  backprop network learningRate error =
-    sequence backprop' error (reverse $ children network) network
-    where backprop' error = backprop error learningRate
+{-instance Layer a => Layer (SequentialNet a) where-}
+  {-size = sizeSN-}
+  {-feedThru network input =-}
+    {-(net { children = reverse $ children net }, output)-}
+    {-where (net, output) = sequence feedThru input (children network) network-}
+  {-backprop network learningRate error =-}
+    {-sequence backprop' error (reverse $ children network) network-}
+    {-where backprop' error = backprop error learningRate-}
 
 
 data LinearLayer = LinearLayer { sizeLinearLayer :: Int
@@ -142,19 +181,17 @@ linearLayer sizeIn sizeOut = LinearLayer
   , weights         = randomArray (sizeIn + 1) sizeOut }
 
 
-instance Network LinearLayer where
-  size = sizeLinearLayer
-  feedThru layer input =
-    (layer { input = Just input' }, input' * (weights layer))
-    where input' = addOnes input
-  backprop layer learnRate error =
-      (layer', error * (transpose $ weights layer))
-      where layer'   = layer { weights = weights' }
-            weights' = addGradients learnRate (gradient layer error) layer
+instance Layer LinearLayer where
+  feedThru layer input = addOnes input * (weights layer)
+  backprop layer _ error = error * (transpose $ weights layer)
 
-instance Weighted LinearLayer where
-    getWeights           = weights
-    gradient layer error = (transpose . ifInitialized $ input layer) * error
+      {-where layer'   = layer { weights = weights' }-}
+            {-weights' = addGradients learnRate (gradient layer error) layer-}
+
+instance Learner LinearLayer where
+    getWeights                 = weights
+    setWeights layer weights'  = layer { weights = weights' }
+    gradient layer input error = (transpose input) * error
 
 
 data Sigmoid = Sigmoid { sigmoidInput :: Maybe Input
@@ -164,41 +201,38 @@ sigmoid :: Int -> Sigmoid
 sigmoid size = Sigmoid { sigmoidInput = Nothing
                        , sigmoidSize  = size }
 
-instance Network Sigmoid where
-  size = sigmoidSize
-  feedThru sigmoid input = (sigmoid { sigmoidInput = Just input }
-                           , rmap (\ x -> 1 / (1 + exp (-x))) input)
-  backprop sigmoid _ error = (sigmoid, computeS derivative)
-      where activation = ifInitialized $ sigmoidInput sigmoid
-            derivative = zipWith (\ e a -> e * a * (1 - a)) error activation
+instance Layer Sigmoid where
+  feedThru sigmoid input = rmap (\ x -> 1 / (1 + exp (-x))) input
+  backprop sigmoid input error = computeS derivative
+      where derivative = zipWith (\ e i -> e * i * (1 - i)) error input
 
-data RecurrentNet a = RecurrentNet { sizeRN :: Int
-                                   , output :: Output
-                                   , core   :: a }
+{-data RecurrentNet a = RecurrentNet { sizeRN :: Int-}
+                                   {-, output :: Output-}
+                                   {-, core   :: a }-}
 
-{-instance Network a => Network (RecurrentNet a) where-}
+{-instance Layer a => Layer (RecurrentNet a) where-}
   {-size = sizeRN-}
   {-feedThru RecurrentNet { output, core } input = feedThru core input-}
     {-where input = computeS $ input ++ output-}
   {-backprop network = backprop core-}
 
--- CODE FOR TESTS --
+{--- CODE FOR TESTS ---}
 
-sequentialNetFromMatrices :: [Matrix] -> SequentialNet LinearLayer
-sequentialNetFromMatrices matrices =
-    network { children = map linearLayerFromMatrix matrices }
-    where network = sequentialNet linearLayer [linearLayer 2] 2 2
+{-sequentialNetFromMatrices :: [Matrix] -> SequentialNet LinearLayer-}
+{-sequentialNetFromMatrices matrices =-}
+    {-network { children = map linearLayerFromMatrix matrices }-}
+    {-where network = sequentialNet linearLayer [linearLayer 2] 2 2-}
 
-linearLayerFromValues :: [[Double]] -> LinearLayer
-linearLayerFromValues values =
-    (linearLayer sizeIn sizeOut) { weights = matrix values }
-    where sizeIn  = length values
-          sizeOut = length $ values !! 0
+{-linearLayerFromValues :: [[Double]] -> LinearLayer-}
+{-linearLayerFromValues values =-}
+    {-(linearLayer sizeIn sizeOut) { weights = matrix values }-}
+    {-where sizeIn  = length values-}
+          {-sizeOut = length $ values !! 0-}
 
-linearLayerFromMatrix :: Matrix -> LinearLayer
-linearLayerFromMatrix matrix =
-    (linearLayer sizeIn sizeOut) { weights = matrix }
-    where Z :. sizeIn :. sizeOut = extent matrix
+{-linearLayerFromMatrix :: Matrix -> LinearLayer-}
+{-linearLayerFromMatrix matrix =-}
+    {-(linearLayer sizeIn sizeOut) { weights = matrix }-}
+    {-where Z :. sizeIn :. sizeOut = extent matrix-}
 
-cost :: Output -> Targets -> Error
-cost = (-)
+{-cost :: Output -> Targets -> Error-}
+{-cost = (-)-}
